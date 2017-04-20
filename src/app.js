b9c7de5641f67da599f4c22ebed7de4351ac1d14
Samuel Lee <evo0705@gmail.com>
2017-04-20 12:44:35 +0800

    remove web process, only worker

--- src/app.js
+++ src/app.js
@@ -30,10 +30,10 @@ const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
 queue.process('email', 20, function (job, done) {
     mg.messages.create(process.env.MAILGUN_DOMAIN, {
         from: "Excited User <postmaster@sandbox89efefeaf46d4945816a53f124d0c24d.mailgun.org>",
-        to: ["samuel.lee@jetspree.com"],
-        subject: "Hello",
-        text: "Testing some Mailgun awesomness!",
-        html: "<h1>Testing some Mailgun awesomness!</h1>"
+        to: [job.data.to],
+        subject: job.data.subject,
+        text: job.data.content,
+        html: "<h1>" + job.data.content + "</h1>"
     });
     done();
 });
